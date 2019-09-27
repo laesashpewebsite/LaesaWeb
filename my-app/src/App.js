011a53ea9a8c956a4004f07af0baf1d0233f7ed5
TopSM <ostavo.p@gmail.com>
2019-09-27 18:19:32 -0400

    edits to the main page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './pictures/LaesaShpe.svg';
+import logo from './pictures/LAESA_LOGO.png';
 import './App.css';
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenidos
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          this link is to Learn React
         </a>
       </header>
     </div>
