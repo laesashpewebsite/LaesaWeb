497e2425ac3d775cf5c5510da6887ef690754d80
alexguerrero11 <alex.guerrero178@gmail.com>
2019-11-08 00:59:50 -0500

    I was able to fix the size of the picture
    The group picture issue was fixed. Working on picking a color scheme now for the background.

--- my-app/src/Main.jsx
+++ my-app/src/Main.jsx
@@ -6,12 +6,17 @@ import PCED_group from './pictures/PCED-GROUP.JPG';
 function main() {
   return (
   <div className="Main">
-
+  
   <div className="PCED-GROUP-PIC">
-      <img src={PCED_group} className="PCED-GROUP-PIC;" alt="PCED-GROUP pictures" />
+      <img src={PCED_group} className="PCED-GROUP-PIC" alt="PCED-GROUP pictures" />
   </div>
 
+  <hr className="hr1"/>
+
+  /*Starting slide show about 5 pillars here!!*/
+
   </div> 
+
   );
 }
 export default main;
