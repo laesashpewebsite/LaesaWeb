bb7022c6141ace513c2b27939788e86cd2e8c293
alexguerrero11 <alex.guerrero178@gmail.com>
2019-11-07 20:55:47 -0500

    pced picture was attached
    pced picture

--- my-app/src/Main.jsx
+++ my-app/src/Main.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import logo from './pictures/LAESA_LOGO.png';
-import './Main.css'
+import './Main.css';
+import PCED_group from './pictures/PCED-GROUP.JPG';
 
 function main() {
   return (
-    <div className="Main">
-      <header className="Main-header">
-        <img src={logo} className="Main-logo" alt="logo" />
-        <p className='Main-title'>
-          LAESA-SHPE WEBSITE COMING SOON!
-        </p>
-      </header>
-    </div>
+  <div className="Main">
+
+  <div classname="PCED-GROUP-PIC">
+    <body>
+      <img src={PCED_group} alt="PCED-GROUP pictures" />
+    </body>
+  </div>
+
+  </div> 
   );
 }
 export default main;
