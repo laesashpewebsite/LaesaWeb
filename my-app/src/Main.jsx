891676a454040eb3af0f58842c2b606b8fb7ae69
alexguerrero11 <alex.guerrero178@gmail.com>
2019-11-11 23:31:01 -0500

    included our mission and vision
    Color scheme still needs to be determined.

--- my-app/src/Main.jsx
+++ my-app/src/Main.jsx
@@ -13,6 +13,20 @@ function main() {
 
   <hr className="hr1"/>
 
+  <div>
+ 	<body> 
+  	<h1 className="header"> LAESA SHPE'S MISSION </h1>
+  	<p className="paragraph"> Our mission is to be the premier facilitator of student's endeavors in the Science,
+        Technology, Engineering, and Mathematics field (STEM). Also, to facilitate in other areas
+ 	      as well , as to help to fully develop individuals to their highest potential." 
+ 	  </p>
+ 	  <h1 className="header"> LAESA SHPE'S VISION </h1>
+ 	  <p className="paragraph"> Our mission is to S.T.E.P into THE FUTURE. This can be accomplished by using four 
+      principles. They are the principles of Support, Teamwork, Excellence, and Professionalism!
+ 	  </p>
+  </body> 
+  </div> 
+
   /*Starting slide show about 5 pillars here!!*/
 
   </div> 
