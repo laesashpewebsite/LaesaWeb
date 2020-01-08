79ce322c49e327b418618ce8c3b4c0ccd7825971
alexguerrero11 <alex.guerrero178@gmail.com>
2020-01-07 20:10:54 -0500

    minor edits to fll page
    fll page

--- my-app/src/FLL.jsx
+++ my-app/src/FLL.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
-import construct from './pictures/under-construction.png';
-import './Eboard.css'
+import './FLL.css';
+import director from'./pictures/Jean-Luc.jpg'
 
 function FLL() {
   return (
-    <div className="Eboard-main">
-   		<header className="Eboard-header">
-        <img src={construct} className="constructPic" alt="logo" />
-        <p className='Main-title'>
-        </p>
-      </header>
+    <div className="Main">
+   		<body> 
+  	<h1 className="header"> FLL'S MISSION </h1>
+  	<p className="paragraph">
+  	First Lego League's mission is to inspire young adults to be science and technology innovators
+  	, by engaging them in exciting programs that help them build the necessaries skills in science,
+  	engineering, and technology.
+ 	  </p>
+ 	  <h1 className="header"> Our Director </h1>
+ 	  <img> src={director} </img>
+ 	  <p className="paragraph"> _________________
+ 	  hey
+ 	  </p>
+ 	  <p className="paragraph"> _________________
+ 	  hey
+ 	  </p>
+
+  </body>
+
     </div>
+
   );
 }
 export default FLL;
