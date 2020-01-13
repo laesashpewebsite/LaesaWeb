2a343ba5fef74a00b724d92cdc9c6c2b4b6bfee8
alexguerrero11 <alex.guerrero178@gmail.com>
2020-01-12 21:02:33 -0500

    minor edits to FLL page
    center the directors picture

--- my-app/src/Main.jsx
+++ my-app/src/Main.jsx
@@ -12,32 +12,35 @@ import column4 from './pictures/#4.jpg';
 import column5 from './pictures/#5.jpg';
 
 function main() {
-  return (
-    <div className="Main">
-      <div className="Convetion-Group">
-        <img
-          src={SHPE_Convention}
-          className="Convention-Pic"
-          alt="Convention Group Picture"
-        />
-      </div>
-
+return (
   <div className="Main">
- 	<body> 
+    <div className="Main">
+      <img
+        src={SHPE_Convention}
+        className="Main-Picture"
+        alt="Convention Group Picture"
+      />
+    </div>
+
+  <div>
+ 	  <body> 
   	<h1 className="header"> LAESA SHPE'S MISSION </h1>
-  	<p className="paragraph"> Our mission is to be the premier facilitator of student's endeavors in the Science,
-        Technology, Engineering, and Mathematics field (STEM). Also, to facilitate in other areas
- 	      as well , as to help to fully develop individuals to their highest potential." 
- 	  </p>
+  	   <p className="paragraph"> Our mission is to be the premier facilitator
+        of student's endeavors in the Science,Technology, Engineering, and 
+        Mathematics field (STEM). Also, to facilitate in other areas as well, 
+        as to help to fully develop individuals to their highest potential." 
+ 	    </p>
  	  <h1 className="header"> LAESA SHPE'S VISION </h1>
- 	  <p className="paragraph"> Our mission is to S.T.E.P into THE FUTURE. This can be accomplished by using four 
-      principles. They are the principles of Support, Teamwork, Excellence, and Professionalism!
- 	  </p>
-  </body>
+ 	    <p className="paragraph"> Our mission is to S.T.E.P into THE FUTURE. 
+        This can be accomplished by using four principles. They are the 
+        principles of Support, Teamwork, Excellence, and Professionalism!
+ 	    </p>
+    </body>
+  </div>
 
   <body> 
     <h1 className="header"> LAESA SHPE'S 5 Pillars </h1>
-  <div className="row">
+    <div className="row">
     <div>
      <img src={column} className="column" alt="column #1" />
      <img src={column1} className="column-pop" alt="column #1" />
@@ -55,12 +58,11 @@ function main() {
       <img src={column4} className="column-pop" alt="column #4" />
     </div>
     <div>
-     <img src={column} className="column" alt="column #5" />
+      <img src={column} className="column" alt="column #5" />
       <img src={column5} className="column-pop" alt="column #5" />
     </div>
-  </div>
+    </div>
   </body>
-  </div>
 
 </div>
 );
