195b99c33ee1fd6b814153ca90a87f8f28c65dc2
alexguerrero11 <alex.guerrero178@gmail.com>
2020-01-17 00:33:21 -0500

    minor edits
    minor edits

--- my-app/src/FLL.jsx
+++ my-app/src/FLL.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import './FLL.css';
 import director from './pictures/Jean-Luc.jpg';
+import FLL_Picture1 from './pictures/FLL Pictures/IMG_8317.jpg';
+import FLL_Picture2 from './pictures/FLL Pictures/IMG_8322.jpg';
+import FLL_Picture3 from './pictures/FLL Pictures/IMG_8311.jpg';
+import FLL_Picture4 from './pictures/FLL Pictures/IMG_8316.jpg';
+
 import example from './pictures/Template for 5 pillars.png';
-import './pictures/FLL Pictures';
 
 function FLL() {
   return (
@@ -21,10 +25,12 @@ function FLL() {
 
     <div className="box2">
  	    <h2> About the Director </h2>
+      <div className="Position">
       <img 
       className="Director-Photo"
  	    src={director} 
       alt="Jean-Luc's Profile Picture"/>
+      </div>
  	    <p>
  	    Jean-Luc Antoine is a second-year student at The City College of New York and has been part of 
       LAESA-SHPE since the Spring 2019 semester. He has been particularly involved with FIRST(For 
@@ -40,27 +46,22 @@ function FLL() {
 
     <div className="box1">
       <h2> Pictures </h2>
-      <p>
-      <img 
-      className="Main-Picture"
-      src={example} 
-      alt="Jean-Luc's Profile Picture"/>
-      </p>
-    </div>
-
-    <div className="box2">
       <img 
       className="Director-Photo"
-      src= {director}
+      src= {FLL_Picture1}
       alt="Picture 1"/>
       <img 
       className="Director-Photo"
-      src={director} 
-      alt="Picture 1"/>
+      src={FLL_Picture2} 
+      alt="Picture 2"/>
       <img 
       className="Director-Photo"
-      src={director} 
-      alt="Picture 1"/>
+      src={FLL_Picture3} 
+      alt="Picture 3"/>
+      <img 
+      className="Director-Photo"
+      src={FLL_Picture4} 
+      alt="Picture 4"/>
     </div>
 
     <div className="box2">
