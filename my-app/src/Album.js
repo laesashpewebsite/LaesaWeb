2f4eef33e8535568925ac9b460f844a9fb7dda4d
alexguerrero11 <alex.guerrero178@gmail.com>
2020-03-13 18:25:02 -0400

    update

--- my-app/src/Album.js
+++ my-app/src/Album.js
@@ -13,6 +13,9 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
+import PCED_Director from "./pictures/Arelis.png";
+import PCED_Director2 from "./pictures/Derwick.jpg";
+import './Director.jsx';
 
 function Copyright() {
   return (
@@ -59,7 +62,7 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const cards = [1, 2,3, 4, 5, 6, 7, 8, 9];
 
 export default function Album() {
   const classes = useStyles();
@@ -80,69 +83,117 @@ export default function Album() {
         <div className={classes.heroContent}>
           <Container maxWidth="sm">
             <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-              Album layout
+              Pre College Engineering Day Album
             </Typography>
             <Typography variant="h5" align="center" color="textSecondary" paragraph>
-              Something short and leading about the collection below—its contents, the creator, etc.
-              Make it short and sweet, but not too short so folks don&apos;t simply skip over it
-              entirely.
+              One of LAESA-SHPE's many big event hosted in City College of New York
             </Typography>
-            <div className={classes.heroButtons}>
-              <Grid container spacing={2} justify="center">
-                <Grid item>
-                  <Button variant="contained" color="primary">
-                    Main call to action
-                  </Button>
-                </Grid>
-                <Grid item>
-                  <Button variant="outlined" color="primary">
-                    Secondary action
-                  </Button>
-                </Grid>
-              </Grid>
-            </div>
           </Container>
         </div>
+
         <Container className={classes.cardGrid} maxWidth="md">
           {/* End hero unit */}
           <Grid container spacing={4}>
             {cards.map(card => (
               <Grid item key={card} xs={12} sm={6} md={4}>
+
                 <Card className={classes.card}>
                   <CardMedia
                     className={classes.cardMedia}
-                    image="https://source.unsplash.com/random"
+                    image={PCED_Director}
                     title="Image title"
                   />
                   <CardContent className={classes.cardContent}>
                     <Typography gutterBottom variant="h5" component="h2">
-                      Heading
+                      Arelis Fienco
                     </Typography>
                     <Typography>
-                      This is a media card. You can use this section to describe the content.
+                      Read more about your 26th PCED Director.
                     </Typography>
                   </CardContent>
                   <CardActions>
                     <Button size="small" color="primary">
                       View
                     </Button>
-                    <Button size="small" color="primary">
-                      Edit
-                    </Button>
                   </CardActions>
                 </Card>
+
               </Grid>
             ))}
           </Grid>
         </Container>
+
+        <div className={classes.heroContent}>
+          <Grid container spacing={4}>
+            {cards.map(card => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image={PCED_Director}
+                    title="Image title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Arelis Fienco
+                    </Typography>
+                    <Typography>
+                      Read more about your 26th PCED Director.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      View
+                    </Button>
+                  </CardActions>
+                </Card>
+
+              </Grid>
+            ))}
+            {cards.map(card => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+
+            <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image={this.getPic()}
+                    title="Image title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {this.getTitle()}: {this.getName()}
+                    </Typography>
+                    <Typography>
+                      {this.getBio()}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      View
+                    </Button>
+                  </CardActions>
+                  {this.inc()}
+                </Card>
+              </Grid>
+            ))}
+            </Grid>
+        </div>
+
       </main>
       {/* Footer */}
       <footer className={classes.footer}>
         <Typography variant="h6" align="center" gutterBottom>
-          Footer
+          Director Contact Info
         </Typography>
         <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
-          Something here to give the footer a purpose!
+          P: 917-392-4710
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+          Email: laesa.pced@gmail.com
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+          Personal: arelisfienco@outlook.com
         </Typography>
         <Copyright />
       </footer>
