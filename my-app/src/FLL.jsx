7a1f0151beb473cec19097e3b24db606249dd6fd
alexguerrero11 <alex.guerrero178@gmail.com>
2020-04-10 23:08:48 -0400

    minor edits
    working on implementing the same template as the PCED page

--- my-app/src/FLL.jsx
+++ my-app/src/FLL.jsx
@@ -1,103 +1,415 @@
-import React from 'react';
-import './FLL.css';
-import director from './pictures/Jean-Luc.jpg';
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
+import CameraIcon from "@material-ui/icons/PhotoCamera";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Link from "@material-ui/core/Link";
+import FLL_Director from './pictures/Jean-Luc.jpg';
 import FLL_Picture1 from './pictures/FLL Pictures/IMG_8317.jpg';
 import FLL_Picture2 from './pictures/FLL Pictures/IMG_8322.jpg';
 import FLL_Picture3 from './pictures/FLL Pictures/IMG_8311.jpg';
 import FLL_Picture4 from './pictures/FLL Pictures/IMG_8316.jpg';
 
-import example from './pictures/Template for 5 pillars.png';
+var num = 0;
+const DirectorList = [
+  {
+    Title: "FLL Director",
+    Name: "Jean-Luc Antoine",
+    Picture: FLL_Director,
+    Bio:
+      "Jean-Luc Antoine is a second-year student at The City College of New York and has been part of\
+      LAESA-SHPE since the Spring 2019 semester. He has been particularly involved with FIRST(For\
+      Inspiration and Recognition of Science and Technology) Lego League which is an organization\
+      that creates a platform to support and help keep young elementary kids interested in STEM and\
+      robotics. Jean-Luc was a volunteer in The Spring 2019 semester at the FFL event and now he is\
+      the director for the FIRST Lego League program at LA in which he is organizing, coordinating\
+      and planning FLL events. As part of his duties, Jean-Luc is overseeing logistics, act direct\
+      communication point with event organizers and ensure that the event goes smoothly, as well\
+      as recruit as many volunteers as needed."
+  },
+  {
+    Title: "FLL CoDirector",
+    Name: "Ceasar",
+    Picture: FLL_Director,
+    Bio:
+      "--- TBD ---"
+  }
+];
+const DirectorContactInfo = [
+  {
+    Email: "jeanlucmantoine@gmail.com",
+    Phone: "347-954-7628"
+  }];
 
-function FLL() {
+
+function Copyright() {
   return (
-    <div className="Main">
-   	<header className="header">
-
-    <div className="box1">
-  	 <h2> About </h2>
-      <p>
-      First Lego League's mission is to inspire young adults to be science 
-      and technology innovators, by engaging them in exciting programs that 
-      help them build the necessaries skills in science, engineering, and 
-      technology.
- 	    </p>
-    </div>
-
-    <div className="box2">
-      <h2> Pictures </h2>
-      <img 
-      className="Pictures"
-      src= {FLL_Picture1}
-      alt="Picture 1"/>
-      <img 
-      className="Pictures"
-      src={FLL_Picture2} 
-      alt="Picture 2"/>
-      <img 
-      className="Pictures"
-      src={FLL_Picture3} 
-      alt="Picture 3"/>
-      <img 
-      className="Pictures"
-      src={FLL_Picture4} 
-      alt="Picture 4"/>
-    </div>
-
-    <div className="box1">
-    <div className="Row">
-    <h2> About Our Directors </h2>
-    <div className="Column1">
-      <img 
-      className="Director-Photo"
- 	    src={director} 
-      alt="Jean-Luc's Profile Picture"/>
- 	    <p className="body">
- 	    Jean-Luc Antoine is a second-year student at The City College of New York and has been part of 
-      LAESA-SHPE since the Spring 2019 semester. He has been particularly involved with FIRST(For 
-      Inspiration and Recognition of Science and Technology) Lego League which is an organization
-      that creates a platform to support and help keep young elementary kids interested in STEM and
-      robotics. Jean-Luc was a volunteer in The Spring 2019 semester at the FFL event and now he is 
-      the director for the FIRST Lego League program at LA in which he is organizing, coordinating 
-      and planning FLL events. As part of his duties, Jean-Luc is overseeing logistics, act direct 
-      communication point with event organizers and ensure that the event goes smoothly, as well 
-      as recruit as many volunteers as needed.
- 	    </p>
-    </div>
-    <div className="Column1">
-      <img 
-      className="Director-Photo"
-      src={director} 
-      alt="Jean-Luc's Profile Picture"/>
-      <p className="body">
-      Jean-Luc Antoine is a second-year student at The City College of New York and has been part of 
-      LAESA-SHPE since the Spring 2019 semester. He has been particularly involved with FIRST(For 
-      Inspiration and Recognition of Science and Technology) Lego League which is an organization
-      that creates a platform to support and help keep young elementary kids interested in STEM and
-      robotics. Jean-Luc was a volunteer in The Spring 2019 semester at the FFL event and now he is 
-      the director for the FIRST Lego League program at LA in which he is organizing, coordinating 
-      and planning FLL events. As part of his duties, Jean-Luc is overseeing logistics, act direct 
-      communication point with event organizers and ensure that the event goes smoothly, as well 
-      as recruit as many volunteers as needed.
-      </p>
-    </div>
-    </div>
-  
-    </div>
-
-    <div className="box2">
-      <h3> Director Contact Info </h3>
-      <div className="column1">
-      <ul className="list">
-      <li className="list-requirement">P: 347-954-7628 </li>
-      <li className="list-requirement">Email: jeanlucmantoine@gmail.com </li>
-      </ul>
-      </div>
-      
-    </div>
-        
-    </header>
-    </div>
-
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
   );
 }
-export default FLL;
+
+const useStyles = makeStyles(theme => ({
+  icon: {
+    marginRight: theme.spacing(2)
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6)
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4)
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8)
+  },
+  card: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column"
+  },
+  cardMedia: {
+    paddingTop: "56.25%" // 16:9
+  },
+  cardContent: {
+    flexGrow: 1
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6)
+  }
+}));
+
+
+export default function FLL() {
+  const classes = useStyles();
+
+ function getName() {
+    return DirectorList[num].Name;
+  }
+  function getBio() {
+    return DirectorList[num].Bio;
+  }
+  function getPic() {
+    return DirectorList[num].Picture;
+  }
+  function getTitle() {
+    return DirectorList[num].Title;
+  }
+  function getEmail(){
+    return DirectorContactInfo.Email;
+  }
+  function getPhone(){
+    return DirectorContactInfo.Phone;
+  }
+  function inc() {
+    if (num === 1) num = 0;
+    else num++;
+  }
+
+
+return (
+<React.Fragment>
+ 
+<main>
+
+  <div className={classes.heroContent}>
+  <Container maxWidth="sm">
+    <Typography
+      component="h1"
+      variant="h2"
+      align="center"
+      color="textPrimary"
+      gutterBottom>
+        First Lego League //Events Name 
+    </Typography>
+    <Typography
+      variant="h5"
+      align="center"
+      color="textSecondary"
+      paragraph>
+        One of LAESA-SHPE's many big event hosted in City College of New York
+    </Typography>
+  </Container>
+  </div>
+
+  // <Card>
+  //   <CardMedia
+  //     component="img"
+  //     alt="25th PCED GROUP PIC"
+  //     height="400"
+  //     image={PCED_Group_25}
+  //     title="25th PCED GROUP PIC"/>
+  
+<Container>
+  <Typography variant="h3" align="center" gutterBottom>
+    Our mission
+  </Typography>
+  <Typography>
+    "First Lego League's mission is to inspire young adults to be science\ 
+    and technology innovators, by engaging them in exciting programs that\ 
+    help them build the necessaries skills in science, engineering, and \\
+    technology."
+  </Typography>
+  </Container>
+
+  <div className={classes.heroContent}> 
+  <Typography variant="h3" align="center" gutterBottom>
+  xxth First Lego League 
+  </Typography>
+
+    <Grid container spacing={5}>
+    <Grid container item xs={4} spacing={0}>
+      <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt=" fill out "
+          height="300"
+          image={FLL_Picture1}
+          title=" fill out "/>
+      </CardActionArea>
+      </Card>
+    </Grid>
+
+    <Grid container item xs={4} spacing={0}>
+      <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          component="img"ß
+          alt=" fill out "
+          height="300"
+          image={FLL_Picture2}
+          title=" fill out "/>
+      </CardActionArea>
+      </Card>
+    </Grid>
+    
+    <Grid container item xs={4} spacing={0}>
+      <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt=" fill out "
+          height="300"
+          image={FLL_Picture3}
+          title=" fill out "/>
+      </CardActionArea>
+      </Card>
+    </Grid>
+
+    <Grid container item xs={4} spacing={0}>
+      <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt=" fill out "
+          height="300"
+          image={FLL_Picture4}
+          title=" fill out "/>
+      </CardActionArea>
+      </Card>
+    </Grid>
+
+    </Grid>
+  </div>
+
+  <div> 
+  <Typography variant="h3" align="center" gutterBottom>
+    Our FLL Directors!
+  </Typography>
+
+    <Grid container spacing={3}>
+// co-directos information, such as name, degree, something they would like to share
+    
+    <Grid container item xs={6} spacing={0}>
+      <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="FLL Director's Profile Picture"
+          height="600"
+          image={DirectorList[0].Picture}
+          title="FLL Director's Profile Picture"/>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {DirectorList[0].Name}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {DirectorList[0].Bio}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+    </Grid>
+
+// co-directos information, such as name, degree, something they would like to share
+    
+    <Grid container item xs={6} spacing={0}>
+    <Card className={classes.root} >
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="FLL CoDirector's Profile Picture"
+          height="600"
+          image={DirectorList[1].Picture}
+          title="FLL CoDirector's Profile Picture"/>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {DirectorList[1].Name}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {DirectorList[1].Bio}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      </Card>
+    </Grid>
+    </Grid>
+  </div>
+</main>
+
+// directors footer - their contact information
+
+<footer className={classes.footer}>
+  <Typography variant="h6" align="center" gutterBottom>
+    Director Contact Info //Title
+  </Typography>
+  <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+    {DirectorContactInfo.Phone}
+  </Typography>
+  <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+    {DirectorContactInfo.Email}
+  </Typography>
+  <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+    Personal: arelisfienco@outlook.com
+  </Typography>
+  <Copyright />
+ </footer>
+
+   
+  </React.Fragment>
+  );
+}
+
+
+// import React from 'react';
+// import './FLL.css';
+// import director from './pictures/Jean-Luc.jpg';
+// import FLL_Picture1 from './pictures/FLL Pictures/IMG_8317.jpg';
+// import FLL_Picture2 from './pictures/FLL Pictures/IMG_8322.jpg';
+// import FLL_Picture3 from './pictures/FLL Pictures/IMG_8311.jpg';
+// import FLL_Picture4 from './pictures/FLL Pictures/IMG_8316.jpg';
+
+// import example from './pictures/Template for 5 pillars.png';
+
+// function FLL() {
+//   return (
+//     <div className="Main">
+//    	<header className="header">
+
+//     <div className="box1">
+//   	 <h2> About </h2>
+//       <p>
+//       First Lego League's mission is to inspire young adults to be science 
+//       and technology innovators, by engaging them in exciting programs that 
+//       help them build the necessaries skills in science, engineering, and 
+//       technology.
+//  	    </p>
+//     </div>
+
+//     <div className="box2">
+//       <h2> Pictures </h2>
+//       <img 
+//       className="Pictures"
+//       src= {FLL_Picture1}
+//       alt="Picture 1"/>
+//       <img 
+//       className="Pictures"
+//       src={FLL_Picture2} 
+//       alt="Picture 2"/>
+//       <img 
+//       className="Pictures"
+//       src={FLL_Picture3} 
+//       alt="Picture 3"/>
+//       <img 
+//       className="Pictures"
+//       src={FLL_Picture4} 
+//       alt="Picture 4"/>
+//     </div>
+
+//     <div className="box1">
+//     <div className="Row">
+//     <h2> About Our Directors </h2>
+//     <div className="Column1">
+//       <img 
+//       className="Director-Photo"
+//  	    src={director} 
+//       alt="Jean-Luc's Profile Picture"/>
+//  	    <p className="body">
+//  	    Jean-Luc Antoine is a second-year student at The City College of New York and has been part of 
+//       LAESA-SHPE since the Spring 2019 semester. He has been particularly involved with FIRST(For 
+//       Inspiration and Recognition of Science and Technology) Lego League which is an organization
+//       that creates a platform to support and help keep young elementary kids interested in STEM and
+//       robotics. Jean-Luc was a volunteer in The Spring 2019 semester at the FFL event and now he is 
+//       the director for the FIRST Lego League program at LA in which he is organizing, coordinating 
+//       and planning FLL events. As part of his duties, Jean-Luc is overseeing logistics, act direct 
+//       communication point with event organizers and ensure that the event goes smoothly, as well 
+//       as recruit as many volunteers as needed.
+//  	    </p>
+//     </div>
+//     <div className="Column1">
+//       <img 
+//       className="Director-Photo"
+//       src={director} 
+//       alt="Jean-Luc's Profile Picture"/>
+//       <p className="body">
+//       Jean-Luc Antoine is a second-year student at The City College of New York and has been part of 
+//       LAESA-SHPE since the Spring 2019 semester. He has been particularly involved with FIRST(For 
+//       Inspiration and Recognition of Science and Technology) Lego League which is an organization
+//       that creates a platform to support and help keep young elementary kids interested in STEM and
+//       robotics. Jean-Luc was a volunteer in The Spring 2019 semester at the FFL event and now he is 
+//       the director for the FIRST Lego League program at LA in which he is organizing, coordinating 
+//       and planning FLL events. As part of his duties, Jean-Luc is overseeing logistics, act direct 
+//       communication point with event organizers and ensure that the event goes smoothly, as well 
+//       as recruit as many volunteers as needed.
+//       </p>
+//     </div>
+//     </div>
+  
+//     </div>
+
+//     <div className="box2">
+//       <h3> Director Contact Info </h3>
+//       <div className="column1">
+//       <ul className="list">
+//       <li className="list-requirement">P: 347-954-7628 </li>
+//       <li className="list-requirement">Email: jeanlucmantoine@gmail.com </li>
+//       </ul>
+//       </div>
+      
+//     </div>
+        
+//     </header>
+//     </div>
+
+//   );
+// }
+// export default FLL;
