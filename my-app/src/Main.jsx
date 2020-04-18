60a7832fdb688609e864bdc2735d4bac5e1d8055
TopSM <ostavo.p@gmail.com>
2020-04-18 18:13:17 -0400

    fixed mainpage overlap

--- my-app/src/Main.jsx
+++ my-app/src/Main.jsx
@@ -42,38 +42,37 @@ function main() {
             alumni members, has had many achievements at The City College of New
             York and within the community. Some of our accomplishments are
             listed below:
-            <ul>
-              <h5> Academic Development & Leadership Yearlty Programs </h5>
-              <li className="list">
-                Developing Excellence in Academics and Leadership DEAL Program
-                since 2000
-              </li>
-              <li className="list">
-                Information Technology Program since 1990
-              </li>
-              <li className="list">Mentorship Program since 2018</li>
-              <h5> Community Outreach Programs & Events </h5>
-              <li className="list">Pre-College Engineering Day since 1994</li>
-              <li className="list">
-                New York Institute of Science, Technology, Engineering, and
-                Mathematics since 2001
-              </li>
-              <li className="list">Noche de Ciencas since 2017</li>
-              <li className="list">First Lego League since 2001</li>
-              <li className="list">
-                SHPE Jr. Chapters at Manhattan Bridges High School and George
-                Washington High School since 2019
-              </li>
-              <h5> Leadership Representatives within SHPE </h5>
-              <li className="list">
-                Region IV Regional Student Representative 2001, 2003, 2004, 2018
-              </li>
-              <li className="list">
-                Region IV Sub-Regional Student Representative 2000, 2006, 2018,
-                2019
-              </li>
-            </ul>
           </p>
+
+          <ul>
+            <h5> Academic Development & Leadership Yearlty Programs </h5>
+            <li className="list">
+              Developing Excellence in Academics and Leadership DEAL Program
+              since 2000
+            </li>
+            <li className="list">Information Technology Program since 1990</li>
+            <li className="list">Mentorship Program since 2018</li>
+            <h5> Community Outreach Programs & Events </h5>
+            <li className="list">Pre-College Engineering Day since 1994</li>
+            <li className="list">
+              New York Institute of Science, Technology, Engineering, and
+              Mathematics since 2001
+            </li>
+            <li className="list">Noche de Ciencas since 2017</li>
+            <li className="list">First Lego League since 2001</li>
+            <li className="list">
+              SHPE Jr. Chapters at Manhattan Bridges High School and George
+              Washington High School since 2019
+            </li>
+            <h5> Leadership Representatives within SHPE </h5>
+            <li className="list">
+              Region IV Regional Student Representative 2001, 2003, 2004, 2018
+            </li>
+            <li className="list">
+              Region IV Sub-Regional Student Representative 2000, 2006, 2018,
+              2019
+            </li>
+          </ul>
         </div>
 
         <div className="box1">
