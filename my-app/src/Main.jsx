930f1c431275b5eeb82e0048c047a2cd0a8cb08a
alexguerrero11 <alex.guerrero178@gmail.com>
2020-04-27 16:39:55 -0400

    minor edits main page
    main page's code was commented on

--- my-app/src/Main.jsx
+++ my-app/src/Main.jsx
@@ -20,6 +20,7 @@ import fadeImages4 from './pictures/PillarsFolder/CommunityOutreach.jpg';
 //   './pictures/PillarsFolder/CommunityOutreach.jpg'
 // ];
 
+{/*Properties for the slide show are here*/}
 const fadeProperties = {
       duration: 5000,
       transitionDuration: 500,
@@ -31,97 +32,96 @@ const fadeProperties = {
       }
     };
 
+{/* Main Code starts here */}
 function main() {
-  return (
-    <div className="Main">
-      <header className="header">
-        <div className="box1">
-          <h2> Meet Our Familia! </h2>
-          <img
-            src={SHPE_Convention}
-            className="Main-Picture"
-            alt="Convention Group"
-          />
-        </div>
+return (
+  <div className="Main">
+    <header className="header">
 
-        <div className="box2">
-          <h2> About LAESA-SHPE </h2>
-          <p className="body">
-            LAESA–SHPE is the largest and most prominent undergraduate student
-            organization at The City College of New York. In 1983, a group of
-            dedicated students, with the goal to unite all Latino and other
-            minority engineering students, share cultural ideas and promote
-            leadership, founded LAESA within City College. In 1986, LAESA joined
-            forces with the Society of Hispanic Professional Engineers (SHPE) to
-            better achieve their goals and connect with a larger body of
-            professionals. This partnership has flourished over the years and
-            today, LAESA is a firm supporter of the SHPE core values, while
-            remaining true to its original mission. With over 150 members,
-            LAESA-SHPE has been able to gather motivated students from all
-            engineering disciplines and a diverse pool of cultural backgrounds.
-            This dynamic group of students, alongside their highly supportive
-            alumni members, has had many achievements at The City College of New
-            York and within the community. Some of our accomplishments are
-            listed below:
+{/* Main Picture */}
+  <div className="box1">
+    <h2> Meet Our Familia! </h2>
+    <img src={SHPE_Convention} className="Main-Picture" alt="Convention Group"/>
+  </div>
+
+{/* About LASA-SHPE Information */}
+  <div className="box2">
+    <h2> About LAESA-SHPE </h2>
+      <p className="body">
+        LAESA–SHPE is the largest and most prominent undergraduate student
+        organization at The City College of New York. In 1983, a group of
+        dedicated students, to unite all Latino and other
+        minority engineering students, share cultural ideas and promote
+        leadership, founded LAESA within City College. In 1986, LAESA joined
+        forces with the Society of Hispanic Professional Engineers (SHPE) to
+        better achieve their goals and connect with a larger body of
+        professionals. This partnership has flourished over the years and
+        today, LAESA is a firm supporter of the SHPE core values, while
+        remaining true to its original mission. With over 150 members,
+        LAESA-SHPE has been able to gather motivated students from all
+        engineering disciplines and a diverse pool of cultural backgrounds.
+        This dynamic group of students, alongside their highly supportive
+        alumni members, has had many achievements at The City College of New
+        York and within the community. Some of our accomplishments are
+        listed below:
+      </p>
+    <ul>
+    <h5> Academic Development & Leadership Yearlty Programs </h5>
+      <li className="list">
+        Developing Excellence in Academics and Leadership DEAL Program since 2000
+      </li>
+      <li className="list">Information Technology Program since 1990</li>
+      <li className="list">Mentorship Program since 2018</li>
+    <h5> Community Outreach Programs & Events </h5>
+      <li className="list">Pre-College Engineering Day since 1994</li>
+      <li className="list">
+        New York Institute of Science, Technology, Engineering, and Mathematics since 2001
+      </li>
+      <li className="list">Noche de Ciencas since 2017</li>
+      <li className="list">First Lego League since 2001</li>
+      <li className="list">
+        SHPE Jr. Chapters at Manhattan Bridges High School and George
+        Washington High School since 2019
+      </li>
+    <h5> Leadership Representatives within SHPE </h5>
+      <li className="list">
+        Region IV Regional Student Representative 2001, 2003, 2004, 2018
+      </li>
+      <li className="list">
+        Region IV Sub-Regional Student Representative 2000, 2006, 2018, 2019
+      </li>
+      </ul>
+  </div>
+
+{/* LAESA'S Mission and Vision Section */}
+  <div className="box1">
+    <div className="Row">
+      <div className="Column1">
+        <h2> LAESA SHPE'S MISSION </h2>
+          <p>
+            Our mission is to be the premier facilitator of student's
+            endeavors in the Science, Technology, Engineering, and
+            Mathematics field (STEM). Also, to facilitate in other areas as
+            well, as to help to fully develop individuals to their highest
+            potential."
           </p>
-          <ul>
-            <h5> Academic Development & Leadership Yearlty Programs </h5>
-            <li className="list">
-              Developing Excellence in Academics and Leadership DEAL Program
-              since 2000
-            </li>
-            <li className="list">Information Technology Program since 1990</li>
-            <li className="list">Mentorship Program since 2018</li>
-            <h5> Community Outreach Programs & Events </h5>
-            <li className="list">Pre-College Engineering Day since 1994</li>
-            <li className="list">
-              New York Institute of Science, Technology, Engineering, and
-              Mathematics since 2001
-            </li>
-            <li className="list">Noche de Ciencas since 2017</li>
-            <li className="list">First Lego League since 2001</li>
-            <li className="list">
-              SHPE Jr. Chapters at Manhattan Bridges High School and George
-              Washington High School since 2019
-            </li>
-            <h5> Leadership Representatives within SHPE </h5>
-            <li className="list">
-              Region IV Regional Student Representative 2001, 2003, 2004, 2018
-            </li>
-            <li className="list">
-              Region IV Sub-Regional Student Representative 2000, 2006, 2018,
-              2019
-            </li>
-          </ul>
-        </div>
-
-        <div className="box1">
-          <div className="Row">
-            <div className="Column1">
-              <h2> LAESA SHPE'S MISSION </h2>
-              <p>
-                Our mission is to be the premier facilitator of student's
-                endeavors in the Science,Technology, Engineering, and
-                Mathematics field (STEM). Also, to facilitate in other areas as
-                well, as to help to fully develop individuals to their highest
-                potential."
-              </p>
-              <img className="Column-pictures1" src={Mission} alt="Mission" />
-            </div>
-            <div className="Column1">
-              <h2> LAESA SHPE'S VISION </h2>
-              <p>
-                Our mission is to S.T.E.P into THE FUTURE. This can be
-                accomplished by using four principles. They are the principles
-                of Support, Teamwork, Excellence, and Professionalism!
-              </p>
-              <img className="Column-pictures1" src={Vision} alt="Vision" />
-            </div>
-          </div>
-        </div>
+          <img className="Column-pictures1" src={Mission} alt="Mission" />
+      </div>
+      <div className="Column1">
+        <h2> LAESA SHPE'S VISION </h2>
+          <p>
+            Our mission is to S.T.E.P into THE FUTURE. This can be
+            accomplished by using four principles. They are the principles
+            of Support, Teamwork, Excellence, and Professionalism!
+          </p>
+          <img className="Column-pictures1" src={Vision} alt="Vision" />
+      </div>
+    </div>
+  </div>
     
-{/* Slide show starts here */}
-{/* To change images in slide show just change its origin url. everything else is automatic! */}
+{/* Slide show starts here!! */}
+{/* To change images in slide show just change its origin url. everything else is automatic!
+In addition, the headers also need to be changed manually. */}
     
     <div className="box2">
     <div className="slide-container">
