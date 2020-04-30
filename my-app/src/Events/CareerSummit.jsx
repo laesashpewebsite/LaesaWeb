30f3bebedd17b73abae0f745dbe9a05e4a5a3785
TopSM <ostavo.p@gmail.com>
2020-04-29 22:29:51 -0400

    change pictures name in career summit

--- my-app/src/Events/CareerSummit.jsx
+++ my-app/src/Events/CareerSummit.jsx
@@ -2,17 +2,15 @@ import React, { Component } from "react";
 import "./Events.css";
 
 import CS_Director from "../pictures/Kayley.jpg";
-import CS_DIrector2 from "../pictures/Valentin.jpg";
-// import PCEDPicture1 from "../pictures/PCED_Pictures/Students.jpg";
-// import PCEDPicture2 from "../pictures/PCED_Pictures/Directors2019.jpg";
-// import PCEDPicture3 from "../pictures/PCED_Pictures/Workshop.jpg";
-// import PCEDPicture4 from "../pictures/PCED_Pictures/Volunteers.jpg";
-// import PCEDPicture5 from "../pictures/PCED_Pictures/Volunteers2.jpg";
-// import PCEDPicture6 from "../pictures/PCED_Pictures/Volunteers3.jpg";
-// import PCEDPicture7 from "../pictures/PCED_Pictures/Winners.jpg";
-// import PCEDPicture8 from "../pictures/PCED_Pictures/Teams.jpg";
-// import PCEDPicture9 from "../pictures/PCED_Pictures/Competition.jpg";
-import Main from "../pictures/PillarsFolder/ProfessionalDevelopment.jpg";
+import CS_Director2 from "../pictures/Valentin.jpg";
+
+import CS_Picture1 from "../pictures/CS_Pictures/members.jpg";
+import CS_Picture2 from "../pictures/CS_Pictures/members2.jpg";
+import CS_Picture3 from "../pictures/CS_Pictures/Rep.jpg";
+import CS_Picture4 from "../pictures/CS_Pictures/Sponsor.jpg";
+import CS_Picture5 from "../pictures/CS_Pictures/Sponsor2.jpg";
+
+import Main from "../pictures/CS_pictures/DSC_0350.JPG";
 
 import CardProfiles from "./CardProfiles";
 class CareerSummit extends Component {
@@ -35,7 +33,7 @@ class CareerSummit extends Component {
         id: 2,
         Title: "Career Summit Co-Director",
         Name: "Valentin Martinez",
-        Picture: CS_DIrector2,
+        Picture: CS_Director2,
         Bio:
           "My name is Valentin Martinez a Civil Engineering Graduate 19’ with a focus on Structural Engineering. I’ve been involved with LAESA-SHPE since my sophomore year and have held multiple positions within the organization: FIRST Lego League Director 17’, SHPE Jr. Director 17’, Internal Vice President 18’, and the Career Summit Director 19’.  LAESA-SHPE changes lives by impacting and influencing the Hispanic community, and building you up to become a professional leader. And, I’ve had the privilege to be part of this movement #SHPE4LIFE",
         styles: {
@@ -43,64 +41,39 @@ class CareerSummit extends Component {
           color: "#f26534"
         }
       }
+    ],
+    CS_Pictures: [
+      {
+        img: CS_Picture1,
+        title: "Career Summit Image1",
+        author: "N/A",
+        cols: 1
+      },
+      {
+        img: CS_Picture2,
+        title: "Career Summit Image2",
+        author: "N/A",
+        cols: 2
+      },
+      {
+        img: CS_Picture3,
+        title: "Career Summit Image3",
+        author: "N/A",
+        cols: 3
+      },
+      {
+        img: CS_Picture4,
+        title: "Career Summit Image4",
+        author: "N/A",
+        cols: 3
+      },
+      {
+        img: CS_Picture5,
+        title: "Career Summit Image5",
+        author: "N/A",
+        cols: 3
+      }
     ]
-    //,
-    //  PCEDPictures: [
-    //   {
-    //     img: PCEDPicture1,
-    //     title: "PCED Image1",
-    //     author: "N/A",
-    //     cols: 1
-    //   },
-    //   {
-    //     img: PCEDPicture2,
-    //     title: "PCED Image2",
-    //     author: "N/A",
-    //     cols: 2
-    //   },
-    //   {
-    //     img: PCEDPicture3,
-    //     title: "PCED Image3",
-    //     author: "N/A",
-    //     cols: 3
-    //   },
-    //   {
-    //     img: PCEDPicture4,
-    //     title: "PCED Image4",
-    //     author: "N/A",
-    //     cols: 3
-    //   },
-    //   {
-    //     img: PCEDPicture5,
-    //     title: "PCED Image5",
-    //     author: "N/A",
-    //     cols: 3
-    //   },
-    //   {
-    //     img: PCEDPicture6,
-    //     title: "PCED Image6",
-    //     author: "N/A",
-    //     cols: 3
-    //   },
-    //   {
-    //     img: PCEDPicture7,
-    //     title: "PCED Image7",
-    //     author: "N/A",
-    //     cols: 3
-    //   },
-    //   {
-    //     img: PCEDPicture8,
-    //     title: "PCED Image8",
-    //     author: "N/A",
-    //     cols: 3
-    //   },
-    //   {
-    //     img: PCEDPicture9,
-    //     title: "PCED Image9",
-    //     author: "N/A",
-    //     cols: 3
-    //   }
-    // ]
   };
   render() {
     return (
@@ -116,41 +89,32 @@ class CareerSummit extends Component {
             />
           </div>
 
-
           <div className="box2">
-    <div className="Row">
-
-    <div className="Column1">
-            <h2> About </h2>
-            <p>
-              {" "}
-              The Career Summit is a mock conference for many of our members and
-              other students at CCNY we host this event so our members can get
-              practice talking to recruiters and getting their resume's
-              critiqued by real recruiters in the industry that we bring over
-              for thie event.
-            </p>
+            <div className="Row">
+              <div className="Column1">
+                <h2> About </h2>
+                <p>
+                  {" "}
+                  The Career Summit is a mock conference for many of our members
+                  and other students at CCNY we host this event so our members
+                  can get practice talking to recruiters and getting their
+                  resume's critiqued by real recruiters in the industry that we
+                  bring over for thie event.
+                </p>
+              </div>
+              <div className="Column1">
+                <h2>Mission</h2>
+                <p>
+                  "Our mission is to prepare our members for the real world and
+                  become better candidates when applying for jobs/internships by
+                  helping them improve on their public speaking, resume, or
+                  interview skills"
+                </p>
+              </div>
+            </div>
           </div>
-          <div className="Column1">
-            <h2>Mission</h2>
-            <p>
-              "Our mission is to prepare our members for the real world and
-              become better candidates when applying for jobs/internships by
-              helping them improve on their public speaking, resume, or
-              interview skills"
-            </p>
-          </div>
-          </div>
-          </div>
 
-
-          <div className="card-color2">
-            {/* <h2>Gallery</h2>
-
-            {/* this here displays the picture that are in the array FFLPictures
-            by inputing it into Component GalleryPic */}
-            {/* /   <GalleryPic tileData={this.state.PCEDPictures} /> */}
-          </div>{" "}
+          <div className="card-color2"></div>
           <div className="card-color">
             <h2>Meet Our Directors!</h2>
           </div>
@@ -159,12 +123,14 @@ class CareerSummit extends Component {
             DirectorListName={this.state.DirectorList}
             key={this.state.DirectorList.id}
           />
-          
+
           <div className="card-color2">
             <h2>Director Contact Info</h2>
             <ul className="lists">
               <li className="events-list">P: 917-392-4710 </li>
-              <li className="events-list">Email: laesa.pced@gmail.com </li>
+              <li className="events-list">
+                Email: laesa.Career Summit@gmail.com{" "}
+              </li>
               <li className="events-list">
                 Personal: arelisfienco@outlook.com
               </li>
